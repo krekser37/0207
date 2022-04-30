8b6263bbdec47ff17728c99e1fdbf9ca81a902f8
Ekaterina Kryuchkova <krekser37@yandex.ru>
2022-04-30 21:50:50 +0300

    nat

---
+++ nat/php/lk_obr.php
@@ -0,0 +1,15 @@
+<?php session_start();
+header('Content-Type: text/html; charset=utf-8');
+
+$mysqli = mysqli_connect("localhost", "mhuhzmvm_0207", "12345", "mhuhzmvm_0207");
+
+if ($mysqli == false) {
+  print("error");
+} else {
+$inputValue=$_POST['value'];
+$item=$_POST['item'];
+$id=$_SESSION['id'];
+$mysqli->query("UPDATE `users` SET `$item` = '$inputValue' WHERE `id` = '$id'");
+
+$_SESSION[$item]=$inputValue;
+}
